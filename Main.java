70bbc54dde7881e6927bd83eed53924361a9f5da
Tamicktom <rickpokemon38@gmail.com>
2024-08-17 04:48:03 -0300

    better brazilian states enum

--- Main.java
+++ Main.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 import enums.BrazilianState;
 
 public class Main {
@@ -5,5 +7,17 @@ public class Main {
     BrazilianState state1 = BrazilianState.SP;
 
     System.out.println(state1.getName());
+    System.out.println(state1.getAbbreviation());
+
+    List<BrazilianState> states = BrazilianState.getStates();
+
+    for (BrazilianState state : states) {
+      String name = state.getName();
+      String abbreviation = state.getAbbreviation();
+
+      String message = String.format("State: %s (%s)", name, abbreviation);
+
+      System.out.println(message);
+    }
   }
 }
--- enums/BrazilianState.java
+++ enums/BrazilianState.java
@@ -1,41 +1,62 @@
 package enums;
 
+import java.util.List;
+
 public enum BrazilianState {
-  AC("Acre"),
-  AL("Alagoas"),
-  AP("Amapá"),
-  AM("Amazonas"),
-  BA("Bahia"),
-  CE("Ceará"),
-  DF("Distrito Federal"),
-  ES("Espírito Santo"),
-  GO("Goiás"),
-  MA("Maranhão"),
-  MT("Mato Grosso"),
-  MS("Mato Grosso do Sul"),
-  MG("Minas Gerais"),
-  PA("Pará"),
-  PB("Paraíba"),
-  PR("Paraná"),
-  PE("Pernambuco"),
-  PI("Piauí"),
-  RJ("Rio de Janeiro"),
-  RN("Rio Grande do Norte"),
-  RS("Rio Grande do Sul"),
-  RO("Rondônia"),
-  RR("Roraima"),
-  SC("Santa Catarina"),
-  SP("São Paulo"),
-  SE("Sergipe"),
-  TO("Tocantins");
+  AC("Acre", "AC"),
+  AL("Alagoas", "AL"),
+  AP("Amapá", "AP"),
+  AM("Amazonas", "AM"),
+  BA("Bahia", "BA"),
+  CE("Ceará", "CE"),
+  DF("Distrito Federal", "DF"),
+  ES("Espírito Santo", "ES"),
+  GO("Goiás", "GO"),
+  MA("Maranhão", "MA"),
+  MT("Mato Grosso", "MT"),
+  MS("Mato Grosso do Sul", "MS"),
+  MG("Minas Gerais", "MG"),
+  PA("Pará", "PA"),
+  PB("Paraíba", "PB"),
+  PR("Paraná", "PR"),
+  PE("Pernambuco", "PE"),
+  PI("Piauí", "PI"),
+  RJ("Rio de Janeiro", "RJ"),
+  RN("Rio Grande do Norte", "RN"),
+  RS("Rio Grande do Sul", "RS"),
+  RO("Rondônia", "RO"),
+  RR("Roraima", "RR"),
+  SC("Santa Catarina", "SC"),
+  SP("São Paulo", "SP"),
+  SE("Sergipe", "SE"),
+  TO("Tocantins", "TO");
 
   private String name;
+  private String abbreviation;
 
-  BrazilianState(String name) {
+  BrazilianState(String name, String abbreviation) {
     this.name = name;
+    this.abbreviation = abbreviation;
   }
 
   public String getName() {
-    return name;
+    return this.name;
+  }
+
+  public String getAbbreviation() {
+    return this.abbreviation;
+  }
+
+  public static List<BrazilianState> getStates() {
+    return List.of(BrazilianState.values());
+  }
+
+  public static BrazilianState getStateByAbbreviation(String abbreviation) {
+    for (BrazilianState state : BrazilianState.values()) {
+      if (state.getAbbreviation().equals(abbreviation)) {
+        return state;
+      }
+    }
+    return null;
   }
 }
