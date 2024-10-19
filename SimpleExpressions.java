06b5b5138c69d5a176b12c5e39005961f088649e
Tamicktom <rickpokemon38@gmail.com>
2024-10-19 17:47:18 -0300

    ok

--- SimpleExpressions.java
+++ SimpleExpressions.java
@@ -1,13 +1,17 @@
+import java.time.LocalDate;
+
 public class SimpleExpressions {
   public static void main(String[] args) {
-    String name = "Tamick";
-    String secondName = "Tom";
+    String name = "Henrique";
+    String secondName = "Angelo Vieira Fonseca";
     String fullName = "%s %s".formatted(name, secondName);
+
+    LocalDate beginDate = LocalDate.of(2020, 1, 1);
 
     Integer number = 10;
     Double salary = 100.0;
 
-    String printedText = String.format("Hello %-30s %d %f", fullName, number, salary);
+    String printedText = String.format("Hello %-24.24s %d %f %td/%<tm/%<tY", fullName, number, salary, beginDate);
 
     System.out.println(printedText);
   }
